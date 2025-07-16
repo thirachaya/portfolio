6df623d5012f17fb7825ff8b586951c998753669
Teerachaya <chaya.fact@gmail.com>
2025-07-16 19:56:23 +0700

    Finish HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+'use client';
+
 import HeroSection from "@/components/HeroSection";
 import ThreeDCardScene from "@/components/ThreeDCard/index";
+import AboutMe from "@/components/AboutMe";
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [isMobile, setIsMobile] = useState(false);
+  
+    useEffect(() => {
+      const checkIsMobile = () => {
+        setIsMobile(window.innerWidth < 768);
+      };
+      checkIsMobile();
+      window.addEventListener('resize', checkIsMobile);
+  
+      return () => {
+        window.removeEventListener('resize', checkIsMobile);
+      };
+    }, []);
+
   return (
-    <main className="relative min-h-screen">
+    <main className={`relative min-h-screen flex ${isMobile ? 'justify-start' : 'justify-center'}`}>
         <HeroSection />
         <ThreeDCardScene/>
+        <AboutMe/>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,11 +18,11 @@ export default function HeroSection() {
   }, []);
 
   return (
-    <section className={`${isMobile ? 'text-left' : 'text-center'} px-4 py-20 ${isMobile && 'w-[60%]'}`}>
+    <section className={`${isMobile ? 'text-left' : 'text-center'} px-4 py-20 ${isMobile ? 'w-[60%]' : 'w-[50%]'}  h-[30%]`}>
       <h1 className="text-xl tracking-wide">Hello, I am</h1>
       <p className={`${isMobile ? 'text-2xl' : 'text-5xl'} font-medium tracking-[0.1em]`}>Teerachaya Ngaesanthea</p>
       <p className={`mt-4 ${isMobile ? 'text-lg' : 'text-xl'}`}>
-        Full-Stack Developer with a love for UI/UX
+        I’m a Software Developer with goals and a bit of controlled chaos.
       </p>
     </section>
   );
